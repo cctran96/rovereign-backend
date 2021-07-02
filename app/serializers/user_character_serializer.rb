d9d2a3d7862cd7a48f26bc0cd96108470248a708
Chau Tran <cctran96@gmail.com>
2021-07-01 23:56:30 -0700

    serializer + seed edits

--- app/serializers/user_character_serializer.rb
+++ app/serializers/user_character_serializer.rb
@@ -1,8 +1,12 @@
 class UserCharacterSerializer < ActiveModel::Serializer
-  attributes :id, :name, :gold, :level, :experience, :stats, :inventory
+  attributes :id, :name, :gold, :level, :experience, :stats, :inventory, :role
   belongs_to :user
   
   def inventory
     object.inventory_items.map{|i| InventoryItemSerializer.new(i)}
+  end
+
+  def role
+    Character.select("role").find(object.character.id).role
   end
 end
